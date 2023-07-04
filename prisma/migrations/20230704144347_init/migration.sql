e94ed201bb7abfe39f38a99b78e12a373339bca2
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2023-07-04 20:18:37 +0530

    added prisma

---
+++ prisma/migrations/20230704144347_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT
+);
--- src/app/api/products/getPrice/route.ts
+++ src/app/api/products/getPrice/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
+import path from "path";
 import { STORE } from "../../../../config/constants";
 import ProductRepository from "../../../../data/repositories/product_repository";
+import { PrismaClient } from "@prisma/client";
 
 type Body = {
   link: string;
@@ -8,14 +10,26 @@ type Body = {
 };
 
 export async function POST(request: Request) {
+  const prisma = new PrismaClient();
+
+  const a = await prisma.product.findMany();
+
   const { link, store } = (await request.json()) as Body;
 
-  const intervalId = setInterval(async () => {
-    const res = await ProductRepository.getProductPrice({
-      link,
-      store,
-    });
-  }, 2000);
+  // let count = 0;
+  // const intervalId = setInterval(async () => {
+  //   const res = await ProductRepository.getProductPrice({
+  //     link,
+  //     store,
+  //   });
+  //   console.log("res", res);
+
+  //   count++;
+
+  //   if (count === 3) {
+  //     clearInterval(intervalId);
+  //   }
+  // }, 2000);
 
   return NextResponse.json({
     status: true,

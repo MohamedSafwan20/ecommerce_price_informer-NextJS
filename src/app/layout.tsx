e512c9f95770f53fdd9c779d508200768b181857
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2023-07-06 13:33:42 +0530

    added shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,26 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "../components/theme_provider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Ecommerce Price Informer',
-  description: 'Informs about price changes from a ecommerce store.',
-}
+  title: "Ecommerce Price Informer",
+  description: "Informs about price changes from a ecommerce store.",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
+
 export default function Home() {
-  return <main>Hello</main>;
+  const { setTheme } = useTheme();
+
+  return (
+    <main>
+      <Button
+        onClick={() => {
+          setTheme("light");
+        }}
+      >
+        Light
+      </Button>
+      <Button
+        onClick={() => {
+          setTheme("dark");
+        }}
+      >
+        Dark
+      </Button>
+    </main>
+  );
 }

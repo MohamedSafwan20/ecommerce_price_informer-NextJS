055b15057a4a449a9a247d353c778a43de0842c4
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2023-07-21 14:45:28 +0530

    switched db from sqlite to vercel psql

---
+++ prisma/migrations/20230721091036_init/migration.sql
@@ -0,0 +1,31 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "link" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "store" TEXT NOT NULL,
+    "interval" INTEGER NOT NULL,
+    "orderedPrice" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Snapshot" (
+    "id" SERIAL NOT NULL,
+    "time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "price" INTEGER NOT NULL,
+    "productId" INTEGER NOT NULL,
+
+    CONSTRAINT "Snapshot_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_link_key" ON "Product"("link");
+
+-- AddForeignKey
+ALTER TABLE "Snapshot" ADD CONSTRAINT "Snapshot_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;

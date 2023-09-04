8673a45f3f8aeca04f4bb65ec96c8357c5ad5c6b
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2023-09-04 17:21:38 +0530

    fixed 401 issue when calling API from cron-job.org

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/product/listen"],
+  ignoredRoutes: ["/api/product/listen"],
 });
 
 export const config = {
